79e88b2c1c154646c4d58ee9deb045999e4fc0aa
fchichorro <filipechichorro@gmail.com>
2018-10-09 19:57:38 +0300

    added random_walk agent from wolf_sheep model in mesa and created Organism class

--- mesatraits/agents.py
+++ mesatraits/agents.py
@@ -3,6 +3,8 @@ from numpy.random import choice
 
 import random
 
+from mesatraits.random_walk import RandomWalker
+
 class Patch(Agent):
     '''
     A patch of habitat/resource/whatever
@@ -41,7 +43,7 @@ class Patch(Agent):
             (length is number of species)
         """
         species_count = [0 for i in range(no_of_species)]
-            
+        
         for neighbor in neighbors:
             print(type(neighbor))
             if neighbor.grown:
@@ -85,3 +87,80 @@ class Patch(Agent):
         pass
     
     
+
+class Organism(RandomWalker):
+    
+    def __init__(self, unique_id, pos, model, 
+                 
+                 energy_tank,           #maximum amount of energy
+                 current_energy,        #current energy
+                 metabolic_cost,        #energy lost each round
+                 energy_gain_per_patch, #energy gain each time agent feeds
+                 
+                 sexual,                #boolean, sexual individual or not
+                 female,                #boolean, female or not
+                 age,                   #age of the agent (in ticks?)
+                 maturity_age,          #age at which organism can reproduce
+                 longevity,             #maximum lifespan in ticks
+                 patch_affinity,        #list(4), for each habitat type, from 0 to 1
+                 climatic_affinity,     #mean of climatic affinity
+                 climatic_affinity_sd,  #SD of climatic affinity
+                 line_of_sight,         #range of organismal vision
+                 dispersal_speed,       #measure of how many steps agent can move
+                                        #when in search
+                 reproductive_delay,    #compulsory time between reproductive events
+                 offspring_number,      #no of offsprings per reproductive event
+                 moore = True):
+        
+        super.__init__(unique_id, pos, model, moore)
+        
+        self.energy_tank = energy_tank
+        self.current_energy = current_energy
+        self.metabolic_cost = metabolic_cost
+        self.sexual = sexual
+        self.female = random.choice([True, False]) if sexual else True
+        self.maturity_age = maturity_age
+        self.longevity = longevity
+        self.patch_affinity = patch_affinity
+        self.climatic_affinity = climatic_affinity
+        self.climatic_affinity_sd = climatic_affinity_sd
+        self.line_of_sight = line_of_sight
+        self.dispersal_speed = dispersal_speed
+        self.reproductive_delay = reproductive_delay
+        self.offspring_number = offspring_number
+        
+        #other variables
+        self.reproductive_threshold = (2 * energy_tank ) / 3
+        self.min_energy_after_reprod = energy_tank / 3
+    
+    
+    def search_for_food(self):
+        pass
+    
+    def search_for_partner(self):
+        pass
+    
+    def try_feed(self):
+        pass
+    
+    def try_reproduce(self):
+        if self.age > self.maturity_age:
+            neighbors = self.model.grid.get.neighbors(
+                    pos = self.pos, moore = True, include_center = False,
+                    radius = self.line_of_sight)
+            
+            for neighbor in neighbors:
+                if any(type(neighbors)) == Organism:
+                    pass
+    
+    def try_death(self):
+        pass
+    
+    def age(self):
+        pass
+        
+    def try_become_adult(self):
+        pass
+        
+    def step(self):
+        pass
